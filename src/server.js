aca8f74a30736788ddb291828f7914976bd70c7c
Sekhara Pramod <32711469+V-Volte@users.noreply.github.com>
2023-02-23 10:07:38 +0000

    Minor

--- src/server.js
+++ src/server.js
@@ -7,18 +7,22 @@ const PORT = 5500
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use((err, req, res, next) => {
+    res.send("Error: invalid JSON object sent.");
+})
+
 app.get('/', (req, res)=>{
     res.send("Hello, world!")
 })
 
 app.post('/', (req,res)=> {
     console.log(req.body)
-    let data = req.body;
-    let arr = []
-    for(var element in data) {
-        arr += element;
-    }
-    res.send(data);
+    let creds = req.body;
+})
+
+app.post('/requestdata', (req, res) => {
+    let headers = req.headers;
+    
 })
 
 app.listen(PORT);
